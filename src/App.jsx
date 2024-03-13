c4ff076f4d0b9b77cb973e9c2b13c62b890f342f
Zaheen Rakib <rakibzaheen@gmail.com>
2024-03-13 13:04:21 +0600

    Data Load Successfully

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+function App() { 
+  return (
+    <>   
+      <h1>React World Tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
